ce3b0f23f9d36e2db69e3ffcd7eaa74c2bb7f25c
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2026-07-15 20:50:20 +0300

    add test seniors

---
+++ tests/test_core/test_cluster.py
@@ -0,0 +1,13 @@
+
+
+# TODO: Test when allocation is not possible due to job status is not pending (usage is smaller than capacity)
+
+
+# TODO: Test when allocation is not possible due to above capacity usage (status is ok)
+
+
+# TODO: Test when allocation is possible and job status is correct
+
+
+# TODO: Test single job from not_created to pending than allocate,
+#  after run till completion and make sure usage is now zero and all of the status are correct
