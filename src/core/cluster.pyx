ab479805efbdc7f3f19a3db5e3cf8a0f6f2c1e94
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2026-07-15 20:36:28 +0300

    remove unused

--- src/core/cluster.pyx
+++ src/core/cluster.pyx
@@ -1,9 +1,6 @@
 from .machines import Machine
-from .job import Job
+from .job import Job, JobStatus
 
-cpdef enum AllocationStatus:
-	SUCCESS
-	FAILURE
 
 cdef class Cluster:
 	cdef Job* jobs;
@@ -11,16 +8,20 @@ cdef class Cluster:
 	cdef unsigned int time;
 
 
-	cpdef AllocationStatus allocate(self, size_t machine_index, size_t job_index):
+	cpdef bint allocate(self, size_t machine_index, size_t job_index, int time):
 		cdef Machine machine = self.machines[machine_index]
 		cdef Job job = self.jobs[job_index]
 		
 		if job.metadata.status != JobStatus.PENDING:
-			return AllocationStatus.FAILURE
+			return False
 
-		machine.usage
+        if not machine.add_usage(job.usage):
+            return False
 
-		machine.allocate(job)
+        job.metadata.scheduled_at = time
+		job.update_status = JobStatus.RUNNING
+
+		return True
 
 	cpdef unsigned int foward_time(self):
 		self.time += 1 
@@ -30,9 +31,7 @@ cdef class Cluster:
 
 	cdef void foward_running_jobs_time(self):
 		for job in self.jobs:
-			if job.metadata.status == JobStatus.RUNNING:
-				job.forward_time(self.time)
-			# TODO: add all possibleites
+			job.forward_time(self.time)
 			
 	cdef void foward_machines_time(self):
 		for machine in self.machines:
